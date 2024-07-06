4f8d9993dc1574899835a864fccf1d243a91953d
hontrang <tang11392@gmail.com>
2024-07-06 15:08:22 +0700

    test: add login test

--- src/main/java/com/hth/BaseTest.java
+++ src/main/java/com/hth/BaseTest.java
@@ -1,5 +1,54 @@
 package com.hth;
 
-public class BaseTest {
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+
+public class BaseTest {
+    Playwright playwright;
+    Browser browser;
+
+    BrowserContext context;
+    Page page;
+
+    @BeforeClass
+    void launchBrowser() {
+        playwright = Playwright.create();
+        LaunchOptions launchOptions = new BrowserType.LaunchOptions()
+                .setHeadless(false)
+                .setSlowMo(50);
+        browser = playwright.chromium().launch(launchOptions);
+    }
+
+    @AfterClass
+    void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeMethod
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterMethod
+    void closeContext() {
+        context.close();
+    }
+
+    protected void sleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
